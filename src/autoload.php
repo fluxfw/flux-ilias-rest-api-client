b1f1c10d8f8c64c6fbae909d9b701d5296416d64
fluxfw <fluxfw@fluxlabs.ch>
2022-07-25 08:41:08 +0200

    ext

--- src/autoload.php
+++ src/autoload.php
@@ -7,19 +7,10 @@ require_once __DIR__ . "/../libs/flux-ilias-base-api/autoload.php";
 require_once __DIR__ . "/../libs/flux-rest-api/autoload.php";
 
 use FluxIliasRestApiClient\Libs\FluxAutoloadApi\Adapter\Autoload\Psr4Autoload;
-use FluxIliasRestApiClient\Libs\FluxAutoloadApi\Adapter\Checker\PhpExtChecker;
 use FluxIliasRestApiClient\Libs\FluxAutoloadApi\Adapter\Checker\PhpVersionChecker;
 
 PhpVersionChecker::new(
     ">=8.1"
-)
-    ->checkAndDie(
-        __NAMESPACE__
-    );
-PhpExtChecker::new(
-    [
-        "json"
-    ]
 )
     ->checkAndDie(
         __NAMESPACE__
