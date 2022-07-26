0ef7c8d887ac7db383032137eab316f1527101fe
fluxfw <fluxfw@fluxlabs.ch>
2022-07-26 14:16:16 +0200

    env

--- src/Adapter/Api/IliasRestApiClientConfigDto.php
+++ src/Adapter/Api/IliasRestApiClientConfigDto.php
@@ -2,6 +2,8 @@
 
 namespace FluxIliasRestApiClient\Adapter\Api;
 
+use Exception;
+
 class IliasRestApiClientConfigDto
 {
 
@@ -25,11 +27,24 @@ class IliasRestApiClientConfigDto
         ?bool $trust_self_signed_certificate = null,
         ?bool $nginx_server = null
     ) : static {
+        if ($url === "") {
+            throw new Exception("Missing url");
+        }
+        if ($user === "") {
+            throw new Exception("Missing user");
+        }
+        if ($password === "") {
+            throw new Exception("Missing password");
+        }
+        if (($client ?? "") === "") {
+            $client = "default";
+        }
+
         return new static(
             $url,
             $user,
             $password,
-            $client ?? "default",
+            $client,
             $trust_self_signed_certificate ?? false,
             $nginx_server ?? true
         );
@@ -38,14 +53,29 @@ class IliasRestApiClientConfigDto
 
     public static function newFromEnv() : static
     {
+        $url = $_ENV["FLUX_ILIAS_REST_API_CLIENT_URL"] ?? "";
+        if ($url === "") {
+            throw new Exception("Missing FLUX_ILIAS_REST_API_CLIENT_URL");
+        }
+
+        $user = ($_ENV["FLUX_ILIAS_REST_API_CLIENT_USER"] ?? null) ??
+            (($user_file = $_ENV["FLUX_ILIAS_REST_API_CLIENT_USER_FILE"] ?? "") !== "" && file_exists($user_file) ? rtrim(file_get_contents($user_file) ?: "", "\n\r") : "");
+        if ($user === "") {
+            throw new Exception("Missing FLUX_ILIAS_REST_API_CLIENT_USER or FLUX_ILIAS_REST_API_CLIENT_USER_FILE");
+        }
+
+        $password = ($_ENV["FLUX_ILIAS_REST_API_CLIENT_PASSWORD"] ?? null) ??
+            (($password_file = $_ENV["FLUX_ILIAS_REST_API_CLIENT_PASSWORD_FILE"] ?? "") !== "" && file_exists($password_file) ? rtrim(file_get_contents($password_file) ?: "", "\n\r") : "");
+        if ($password === "") {
+            throw new Exception("Missing FLUX_ILIAS_REST_API_CLIENT_PASSWORD or FLUX_ILIAS_REST_API_CLIENT_PASSWORD_FILE");
+        }
+
         return static::new(
-            $_ENV["FLUX_ILIAS_REST_API_CLIENT_URL"],
-            ($_ENV["FLUX_ILIAS_REST_API_CLIENT_USER"] ?? null) ??
-            (($user_file = $_ENV["FLUX_ILIAS_REST_API_CLIENT_USER_FILE"] ?? null) !== null && file_exists($user_file) ? rtrim(file_get_contents($user_file) ?: "", "\n\r") : null),
-            ($_ENV["FLUX_ILIAS_REST_API_CLIENT_PASSWORD"] ?? null) ??
-            (($password_file = $_ENV["FLUX_ILIAS_REST_API_CLIENT_PASSWORD_FILE"] ?? null) !== null && file_exists($password_file) ? rtrim(file_get_contents($password_file) ?: "", "\n\r") : null),
+            $url,
+            $user,
+            $password,
             ($_ENV["FLUX_ILIAS_REST_API_CLIENT_CLIENT"] ?? null) ??
-            (($client_file = $_ENV["FLUX_ILIAS_REST_API_CLIENT_CLIENT_FILE"] ?? null) !== null && file_exists($client_file) ? rtrim(file_get_contents($client_file) ?: "", "\n\r") : null),
+            (($client_file = $_ENV["FLUX_ILIAS_REST_API_CLIENT_CLIENT_FILE"] ?? "") !== "" && file_exists($client_file) ? rtrim(file_get_contents($client_file) ?: "", "\n\r") : null),
             ($trust_self_signed_certificate = $_ENV["FLUX_ILIAS_REST_API_CLIENT_TRUST_SELF_SIGNED_CERTIFICATE"] ?? null) !== null ? in_array($trust_self_signed_certificate, ["true", "1"]) : null,
             ($nginx_server = $_ENV["FLUX_ILIAS_REST_API_CLIENT_NGINX_SERVER"] ?? null) !== null ? in_array($nginx_server, ["true", "1"]) : null
         );
